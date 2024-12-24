b1aeac6adb1f1e49e04b711b17c1f5b7d1c0e638
@yurisa2 <ae_ysa@contractor.indeed.com>
2024-12-24 17:39:56 -0300

    Add video upload functionality to Google Cloud Storage and update .gitignore

--- video_be.py
+++ video_be.py
@@ -1,8 +1,21 @@
 import functions_framework
 from flask import request, jsonify
-import pickle
 import json
 import base64
+from google.cloud import storage
+import dotenv
+import os
+
+dotenv.load_dotenv()
+BUCKET = os.getenv("BUCKET")
+
+
+def upload_to_gcs(bucket_name, source_file_name, destination_blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(source_file_name)
+
 
 @functions_framework.http
 def receive_video(request):
@@ -17,5 +30,7 @@ def receive_video(request):
     with open("imageToSave.webm", "wb") as fh:
         data = data_dict["data"].split(",")[-1]
         fh.write(base64.b64decode(data))
+
+        upload_to_gcs(BUCKET, "imageToSave.webm", "imageToSave.webm")
 
     return jsonify({"message": "Taurine received and pickled successfully."})
